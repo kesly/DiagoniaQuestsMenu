57e9e591a07f776056a59e1560162f60fd72c044
Voltariuss <loic.duboistermoz@gmail.com>
2021-02-21 12:53:41 +0100

    adds .editorconfig and reformats code

--- src/main/java/fr/voltariuss/diagonia/Main.java
+++ src/main/java/fr/voltariuss/diagonia/Main.java
@@ -28,7 +28,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Voltariuss
  */
 public class Main extends JavaPlugin {
-  
+
   @Override
   public void onEnable() {
     System.out.println("Plugin started.");
