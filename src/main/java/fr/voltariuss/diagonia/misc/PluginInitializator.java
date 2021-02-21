68343be9b893f4707ce89466d0bf32891c00031f
Voltariuss <loic.duboistermoz@gmail.com>
2021-02-21 13:56:46 +0100

    adds PluginInitializator class

---
+++ src/main/java/fr/voltariuss/diagonia/misc/PluginInitializator.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2021 Loïc DUBOIS-TERMOZ alias Voltariuss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package fr.voltariuss.diagonia.misc;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.minuskube.inv.InventoryListener;
+
+/**
+ * This class contains methods for {@link JavaPlugin} initialization
+ * ({@link Listener}, {@link Command}).
+ * 
+ * @author Voltariuss
+ */
+public class PluginInitializator {
+
+  /**
+   * This method is charged of {@link JavaPlugin} initialization.
+   * 
+   * @param plugin The targeted {@link JavaPlugin} for initialization.
+   */
+  public static void initializePlugin(JavaPlugin plugin) {
+    registerListeners(plugin);
+    registerCommands(plugin);
+  }
+
+  /**
+   * This method is charged of {@link Listener} registration.
+   * 
+   * @param plugin The targeted {@link JavaPlugin} for registration.
+   */
+  public static void registerListeners(JavaPlugin plugin) {}
+
+  /**
+   * This method is charged of {@link Command} registration.
+   * 
+   * @param plugin The targeted {@link JavaPlugin} for registration.
+   */
+  public static void registerCommands(JavaPlugin plugin) {}
+}
